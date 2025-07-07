0c2a8a767f6dc1a16091e3220016bc99c60fe22f
Nishit Kumar <nishitkumaroll12@gmail.com>
2025-07-07 18:17:53 +0530

    Updated the Info in Webpage

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+// $(document).ready(function(){
+//     $('.navbar-toggler').click(function(){
+//         $(this).find('.navbar-toggler-icon').toggleClass('toggle');
+//     });
+
+
+
 $(document).ready(function(){
     $('.navbar-toggler').click(function(){
         $(this).find('.navbar-toggler-icon').toggleClass('toggle');
     });
+});
